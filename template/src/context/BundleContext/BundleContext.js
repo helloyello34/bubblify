e9356f2975e8a98f47684815f2766437f2500d99
helloyello34 <kristinn34@gmail.com>
2019-03-04 20:29:18 +0000

    some fuckup ithonk

---
+++ template/src/context/BundleContext/BundleContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const BundleContext = React.createContext({});
+
+export const BundleProvider = BundleContext.Provider;
+export const BundleConsumer = BundleContext.Consumer;
