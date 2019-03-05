9da706fa640789b6b3e952e18ba5b0ec70b24b3c
helloyello34 <kristinn34@gmail.com>
2019-03-05 16:43:01 +0000

    sdlfkje

--- template/src/components/BundleItem/BundleItem.js
+++ template/src/components/BundleItem/BundleItem.js
@@ -1,22 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {NavLink} from 'react-router-dom'
 
-const ProductItem = (bundle) => {
-    var link = "/bundles/" + bundle.id
+const ProductItem = (props) => {
+    const { id, name, items } = props
+    var link = "/bundles/" + id
     return (
         <div
-        key={ bundle.id }
+        key={ id }
         className="list-group-item list-group-item-action flex-column align-items-center">
             <NavLink
             exact to={link}
             >
                 <div>
-                    <div className="card-header text-white">{bundle.name}</div>
+                    <div className="card-header text-white">{name}</div>
                 </div>
             </NavLink>
             <button type="button" className="btn btn-primary">Add to cart</button>
         </div>
     );
+};
+
+ProductItem.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    items: PropTypes.array.isRequired
 };
 
 export default ProductItem;
