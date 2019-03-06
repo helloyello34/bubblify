5f77a7f383aeaa1a40dd5d6807da27839b48588c
helloyello34 <kristinn34@gmail.com>
2019-03-06 16:53:55 +0000

    props changed

--- template/src/components/BundleDetailItem/BundleDetailtem.js
+++ template/src/components/BundleDetailItem/BundleDetailtem.js
@@ -4,19 +4,20 @@ import { NavLink } from 'react-router-dom'
 //MUNA proptypes
 
 const BundleDetailItem = (props) => {
-    var link = "/bubbles/" + props.id
+    const {id, name, price, image, addToCart} = props;
+    var link = "/bubbles/" + id
     return (
         <div
-            key={props.id}
+            key={id}
             className="list-group-item list-group-item-action flex-column align-items-center">
-            <NavLink to={link} className="BundleDetailItem" key={props.id}>
+            <NavLink to={link} className="BundleDetailItem" key={id}>
                 <div>
-                    <div className="card-header text-white">{props.name}</div>
-                    <img src={props.image} />
-                    <p className="text-white">kr. {props.price}</p>
+                    <div className="card-header text-white">{name}</div>
+                    <img src={image} />
+                    <p className="text-white">kr. {price}</p>
                 </div>
             </NavLink>
-            <button type="button" className="btn btn-primary" onClick={() => props.addToCart(props)}>Add to cart</button>
+            <button type="button" className="btn btn-primary" onClick={() => addToCart(props)}>Add to cart</button>
         </div>
     );
 };
